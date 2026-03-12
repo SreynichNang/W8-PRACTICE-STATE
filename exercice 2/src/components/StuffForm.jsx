3c03d247b31365326b85b98d9f0eb2328f082083
Sreynich <srey45224@gmail.com>
2026-03-12 15:17:19 +0700

    done ex2

--- exercice 2/src/components/StuffForm.jsx
+++ exercice 2/src/components/StuffForm.jsx
@@ -1,11 +1,29 @@
-export default function StuffForm() {
+import { useState } from "react";
+
+export default function StuffForm({onSubmit}) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  function nameHandler(event) {
+    setName(event.target.value);
+  }
+
+  function priceHandler(event) {
+    setPrice(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    onSubmit(name, price);
+  } 
+
   return (
-    <form className="stuff-form">
+    <form className="stuff-form" onSubmit={submitHandler}>
       <p>Stuff name</p>
-      <input type="search" placeholder="Banana" />
+      <input type="search" placeholder="Banana" onChange={nameHandler}/>
 
       <p>Stuff price</p>
-      <input type="search" placeholder="15" />
+      <input type="search" placeholder="15" onChange={priceHandler}/>
 
       <button>Add Stuff</button>
     </form>
