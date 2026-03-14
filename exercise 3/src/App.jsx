a14cc14f4f743ab01c81af1df10ab8e05c645811
Sreynich <srey45224@gmail.com>
2026-03-14 23:07:37 +0700

    done ex3

--- exercise 3/src/App.jsx
+++ exercise 3/src/App.jsx
@@ -24,6 +24,26 @@ const ORDERS = [
 export default function App() {
   const [orders, setOrders] = React.useState(ORDERS);
 
+  const getTotal = () => {
+    let result = 0;
+    orders.forEach((order) => {
+      result += order.quantity * order.price;
+    });
+
+    return result;
+  };
+
+  const handleQuantityChanged = (product, newQuantity) => {
+    setOrders(
+      orders.map((order) => {
+        if (order.product === product) {
+          return { ...order, quantity: newQuantity };
+        }
+        return order;
+      })
+    );
+  };
+
   return (
     <>
       <header>
@@ -31,10 +51,12 @@ export default function App() {
       </header>
 
       <div className="order-list">
-        <OrderCard></OrderCard>
+        {orders.map((order, index) => (
+          <OrderCard key={index} product={order.product} productPrice={order.price} 
+          quantity={order.quantity} onQuantityChanged={handleQuantityChanged}/>))};
       </div>
 
-      <CheckoutButton total="TODO"></CheckoutButton>
+      <CheckoutButton total={getTotal()}></CheckoutButton>
     </>
   );
 }
